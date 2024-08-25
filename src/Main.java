eee918b69cbbc73505d924ad0ce353a04be0eea7
Mohd Zaid <95207433+hlozaid@users.noreply.github.com>
2024-08-25 11:59:24 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,83 @@
-class Bank {
+class BankAccountInfo {
     private String name;
     private int accountNumber;
     private double balance;
     private long phoneNumber;
 
-    public Bank(String name, int accountNumber, double balance, long phoneNumber) {
+    String getName() {
+        return name;
+    }
+
+    int getAccountNumber() {
+        return accountNumber;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    void setName(String name) {
         this.name = name;
+    }
+
+    void setAccountNumber(int accountNumber) {
         this.accountNumber = accountNumber;
+    }
+
+    void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    public BankAccountInfo(String name, int accountNumber, double balance, long phoneNumber) {
+        this.setName(name);
+        this.accountNumber = accountNumber;
+        this.setBalance(balance);
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void checkBalance() {
+        System.out.println("Your Current Balance is: " + getBalance());
+    }
 
     public void deposit(double amount) {
         this.balance += amount;
-        System.out.println("Deposited " + amount + ". New balance: " + this.balance);
+        System.out.println("Deposited " + amount + " to " + this.getName());
     }
 
     public void withdraw(double amount) {
         if (amount > this.balance) {
-            System.out.println("Insufficient funds. Current balance: " + this.balance);
+            System.out.println("Insufficient balance");
         } else {
             this.balance -= amount;
-            System.out.println("Withdrew " + amount + ". New balance: " + this.balance);
+            System.out.println("Withdrawn " + amount + ", remaining balance: " + this.balance);
         }
     }
 
-    public double getBalance() {
-        return this.balance;
-    }
-
     public void printAccountInfo() {
-        System.out.println("Name: " + this.name);
-        System.out.println("Account Number: " + this.accountNumber);
-        System.out.println("Balance: " + this.balance);
-        System.out.println("Phone Number: " + this.phoneNumber);
+        System.out.println("Name: " + this.getName());
+        System.out.println("Account Number: " + this.getAccountNumber());
+        System.out.println("Balance: " + this.getBalance());
+        System.out.println("Phone Number: " + this.getPhoneNumber());
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Bank myBank = new Bank("Mohd Zaid", 12345678, 1000.00, 9012528862L);
+        BankAccountInfo myBank = new BankAccountInfo("Mohd Zaid", 12345678, 1000.0, 9012528862L);
 
-        System.out.println("Initial Balance: " + myBank.getBalance());
-
-        myBank.deposit(500.00);
-        myBank.withdraw(200.00);
-
-        System.out.println("Final Balance: " + myBank.getBalance());
-
-
+        System.out.println("Initial balance: " + myBank.getBalance());
+        myBank.deposit(500);
+        myBank.withdraw(300);
+        myBank.checkBalance();
+        System.out.println("<---------------------------------------------->");
         myBank.printAccountInfo();
     }
 }
